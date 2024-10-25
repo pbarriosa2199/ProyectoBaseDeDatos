45a1ae5a7702db8e4fb89bf63d5f49e510ddb37b
pbarriosa2199 <149257021+pbarriosa2199@users.noreply.github.com>
2024-10-24 21:34:59 -0600

    Add files via upload

---
+++ ProyectoFinal/proyecto/src/main/java/com/basededatos/proyecto/model/entity/User.java
@@ -0,0 +1,26 @@
+package com.basededatos.proyecto.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Table(name = "users")
+@Entity
+
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUser;
+    
+    private String username;
+    private String nombre;
+    private String apellido;
+    private String password;
+
+    
+}
